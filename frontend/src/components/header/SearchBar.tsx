e0a98b4885eb77adcc9eaa8ef223455aa6432a83
ShakilOfficial <mrshakilhossainofficial@gmail.com>
2024-11-18 21:28:19 +0600

    update the components

--- frontend/src/components/header/SearchBar.tsx
+++ frontend/src/components/header/SearchBar.tsx
@@ -4,22 +4,22 @@ import { Search } from "lucide-react";
 export default function SearchBar({ isMobile }: { isMobile?: boolean }) {
   return (
     <div
-      className={`${
+      className={`flex items-center ${
         isMobile
-          ? "flex w-full m-4"
+          ? "w-full m-4"
           : "hidden md:flex relative md:w-[80px] lg:w-[200px] sm:w-[140px]"
       }`}
     >
-      <Input
-        className={`pl-8 ${isMobile ? "w-full" : ""}`}
-        placeholder="Search products"
-        type="search"
-      />
-      <Search
-        className={`absolute left-2 top-2.5 h-3 w-4 text-muted-foreground ${
-          isMobile ? "hidden" : "block"
-        }`}
-      />
+      <div className="relative w-full">
+        <Search
+          className={`absolute left-2 top-1/2 transform -translate-y-1/2 h-4 w-4 text-muted-foreground`}
+        />
+        <Input
+          className={`pl-8 ${isMobile ? "w-full" : ""} h-8`}
+          placeholder="Search products"
+          type="search"
+        />
+      </div>
     </div>
   );
 }
